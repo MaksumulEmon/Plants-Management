dec7161802a2b1b4889d976f3ae0846fcfffd669
MaksumulEmon <maksumulemon300@gmail.com>
2026-02-28 16:16:07 +0600

    Stuggling Section Add And remove

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 let thriveList = [];
 let strugglingList = [];
 
+let currentStatus = 'all';
+
 let total = document.getElementById('total');
 let thriveCount = document.getElementById('thriveCount');
 let strugglingCount = document.getElementById('strugglingCount');
@@ -34,17 +36,26 @@ function toggleStyle(id) {
     trivingfilterBtn.classList.add('bg-gray-300', 'text-black');
     strugglingFilterBtn.classList.add('bg-gray-300', 'text-black');
 
+
+
     const selected = document.getElementById(id)
+
+    currentStatus = id;
     selected.classList.remove("bg-gray-300", "text-black")
     selected.classList.add("bg-black", "text-white")
 
 
-    if(id =='triving-filter-btn'){
+    if (id == 'triving-filter-btn') {
         allCardSection.classList.add('hidden')
         filterSection.classList.remove('hidden')
-    }else if(id =='all-filter-btn'){
+        renderThriving()
+    } else if (id == 'all-filter-btn') {
         allCardSection.classList.remove('hidden');
         filterSection.classList.add('hidden')
+    } else if (id == 'struggling-filter-btn') {
+        allCardSection.classList.add('hidden')
+        filterSection.classList.remove('hiddem')
+        renderStuggle()
     }
 
 }
@@ -53,8 +64,6 @@ function toggleStyle(id) {
 
 mainContainer.addEventListener('click', function (event) {
 
-
-    console.log(event.target.parentNode.parentNode);
 
     if (event.target.classList.contains('triving-btn')) {
 
@@ -67,8 +76,14 @@ mainContainer.addEventListener('click', function (event) {
         const notes = parentNode.querySelector('.notes').innerText
         //    console.log(plantName,light,water,latinName,status,notes);
 
+        parentNode.querySelector('.status').innerText = 'Trive'
+
         const cardInfo = {
-            plantName, light, water, status, notes
+            plantName,
+            light,
+            water,
+            status: 'Trive',
+            notes
         }
 
         const palntExist = thriveList.find(item => item.plantName == cardInfo.plantName)
@@ -77,8 +92,62 @@ mainContainer.addEventListener('click', function (event) {
             thriveList.push(cardInfo);
         }
 
-        renderThriving();
+        strugglingList = strugglingList.filter(item => item.plantName != cardInfo.plantName)
+
+        calculatCount();
+
+
+        if(currentStatus == 'struggling-filter-btn'){
+
+            renderStuggle()
+        }
+        
     }
+
+
+
+
+    // =========================================================================================
+
+
+    else if (event.target.classList.contains('struggle-btn')) {
+
+        const parentNode = event.target.parentNode.parentNode;
+        const plantName = parentNode.querySelector('.plantName').innerText
+        const latinName = parentNode.querySelector('.latinName').innerText
+        const light = parentNode.querySelector('.light').innerText
+        const water = parentNode.querySelector('.water').innerText
+        const status = parentNode.querySelector('.status').innerText
+        const notes = parentNode.querySelector('.notes').innerText
+        //    console.log(plantName,light,water,latinName,status,notes);
+
+        parentNode.querySelector('.status').innerText = 'Stuggle'
+
+        const cardInfo = {
+            plantName,
+            light,
+            water,
+            status: 'Stuggle',
+            notes
+        }
+
+        const palntExist = strugglingList.find(item => item.plantName == cardInfo.plantName)
+
+        if (!palntExist) {
+            strugglingList.push(cardInfo);
+        }
+
+        thriveList = thriveList.filter(item => item.plantName != cardInfo.plantName)
+
+        if (currentStatus == 'triving-filter-btn') {
+            renderThriving();
+        }
+
+        calculatCount();
+
+    }
+
+
 
 })
 
@@ -108,7 +177,63 @@ function renderThriving() {
                         <p class="water bg-gray-200 px-5">weekly</p>
                     </div>
                     <!-- Part there -->
-                    <p class="status">Not Applicable</p>
+                    <p class="status">${thrive.status}</p>
+                    <p class="notes">New Leaf unfuring by the east windows</p>
+
+                    <div class="flex gap-3">
+                        <button class="triving-btn bg-green-200 px-4 py-2">Thrive</button>
+                        <button class="struggle-btn bg-red-200 px-4 py-2">Struggle</button>
+                    </div>
+                </div>
+                <!-- Main part 2 -->
+                <div>
+                    <button class="btn-delete bg-red-200 text-red-600 px-6 py-2">Delete</button>
+                </div>
+
+        `
+
+        filterSection.appendChild(div);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+// -----------------------------------------------------------------------------------
+
+function renderStuggle() {
+
+    filterSection.innerHTML = ''
+
+    for (let stugle of strugglingList) {
+
+        let div = document.createElement('div');
+        div.className = 'card flex justify-between  shadow-2xl rounded-2xl p-8'
+        div.innerHTML = `
+
+                <!-- main Part 1 -->
+                <div class="space-y-3 text-lg">
+                    <!-- Part one -->
+                    <div>
+                        <p class="plantName text-4xl font-bold">${stugle.plantName}</p>
+                        <p class="latinName">Latin name</p>
+                    </div>
+                    <!-- Part two -->
+                    <div class="flex gap-3">
+                        <p class="light bg-gray-200 px-5"> Bright Indicate</p>
+                        <p class="water bg-gray-200 px-5">weekly</p>
+                    </div>
+                    <!-- Part there -->
+                    <p class="status">${stugle.status}</p>
                     <p class="notes">New Leaf unfuring by the east windows</p>
 
                     <div class="flex gap-3">
